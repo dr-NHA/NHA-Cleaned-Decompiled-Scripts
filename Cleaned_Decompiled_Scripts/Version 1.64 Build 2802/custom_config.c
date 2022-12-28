ad2400ab4a5f7fea444be3bef6896d70a6ad58a1
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2022-12-29 10:15:41 +1030

    custom_config.c Uploaded @ 28/12/2022 11:45:40 PM

---
+++ Cleaned_Decompiled_Scripts/Version 1.64 Build 2802/custom_config.c
@@ -0,0 +1,8 @@
+//Cleaned With dr NHA's C Script Cleaner V2.72
+
+
+void main() // Position - 0x0{
+if(PLAYER::HAS_FORCE_CLEANUP_OCCURRED(3)) SCRIPT::TERMINATE_THIS_THREAD();
+SCRIPT::TERMINATE_THIS_THREAD();
+return;
+}
