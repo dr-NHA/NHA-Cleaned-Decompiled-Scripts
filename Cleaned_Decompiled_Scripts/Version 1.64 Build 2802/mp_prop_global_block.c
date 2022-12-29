5e3beeaa32b72cba4bbaaa1fde6a0c24d0413a3d
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2022-12-29 10:36:42 +1030

    mp_prop_global_block.c Uploaded @ 29/12/2022 12:06:37 AM

---
+++ Cleaned_Decompiled_Scripts/Version 1.64 Build 2802/mp_prop_global_block.c
@@ -0,0 +1,7 @@
+//Cleaned With dr NHA's C Script Cleaner V2.72
+// Program registers 259320 globals at index 5 starting from Global_1310720
+
+void main() // Position - 0x0{
+Global_1570032=-1;
+return;
+}
