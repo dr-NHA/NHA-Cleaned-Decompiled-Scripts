7998a64d9dae19c541c6bde14c2c00c2e84570b2
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2022-12-29 10:40:50 +1030

    scaleformprofiling.c Uploaded @ 29/12/2022 12:10:43 AM

---
+++ Cleaned_Decompiled_Scripts/Version 1.64 Build 2802/scaleformprofiling.c
@@ -0,0 +1,186 @@
+//Cleaned With dr NHA's C Script Cleaner V2.72
+#region Local Var
+var uLocal_0=0;
+var uLocal_1=0;
+int iLocal_2=0;
+int iLocal_3=0;
+int iLocal_4=0;
+int iLocal_5=0;
+int iLocal_6=0;
+int iLocal_7=0;
+int iLocal_8=0;
+int iLocal_9=0;
+int iLocal_10=0;
+int iLocal_11=0;
+var uLocal_12=0;
+var uLocal_13=0;
+float fLocal_14=0f;
+var uLocal_15=0;
+var uLocal_16=0;
+int iLocal_17=0;
+Player plLocal_18=0;
+#endregion
+
+void main() // Position - 0x0{
+iLocal_2=1;
+iLocal_3=134;
+iLocal_4=134;
+iLocal_5=1;
+iLocal_6=1;
+iLocal_7=1;
+iLocal_8=134;
+iLocal_9=1;
+iLocal_10=12;
+iLocal_11=12;
+fLocal_14=0.001f;
+iLocal_17=-1;
+if(CAM::IS_SCREEN_FADED_OUT()) CAM::DO_SCREEN_FADE_IN(500);
+if(PLAYER::HAS_FORCE_CLEANUP_OCCURRED(3)) func_2();
+plLocal_18=GRAPHICS::REQUEST_SCALEFORM_MOVIE("scaleform_profiling");
+while (!GRAPHICS::HAS_SCALEFORM_MOVIE_LOADED(plLocal_18)){
+SYSTEM::WAIT(0);
+}
+while (true){
+GRAPHICS::DRAW_SCALEFORM_MOVIE(plLocal_18, 0.5f, 0.5f, 1f, 1f, 255, 255, 255, 0, 0);
+func_1(plLocal_18);
+SYSTEM::WAIT(0);
+}
+return;
+}
+
+
+void func_1(Player plParam0) // Position - 0x93{
+if(Global_20362==0){
+if(GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 189) || GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 190) || GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 188) || GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 187) || GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 205) || GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 206) || GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 201) || GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 202)){
+Global_20362=1;
+SYSTEM::SETTIMERA(0);
+}}elseif(SYSTEM::TIMERA() > 50){
+Global_20362=0;
+}
+if(Global_20362==0){
+if(GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 189)){
+NETWORK::NETWORK_LEAVE_PED_BEHIND_BEFORE_CUTSCENE(plParam0, "SET_INPUT_EVENT");
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(10);
+GRAPHICS::END_SCALEFORM_MOVIE_METHOD();
+}
+if(GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 190)){
+NETWORK::NETWORK_LEAVE_PED_BEHIND_BEFORE_CUTSCENE(plParam0, "SET_INPUT_EVENT");
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(11);
+GRAPHICS::END_SCALEFORM_MOVIE_METHOD();
+}
+if(GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 205)){
+NETWORK::NETWORK_LEAVE_PED_BEHIND_BEFORE_CUTSCENE(plParam0, "SET_INPUT_EVENT");
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(4);
+GRAPHICS::END_SCALEFORM_MOVIE_METHOD();
+}
+if(GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 206)){
+NETWORK::NETWORK_LEAVE_PED_BEHIND_BEFORE_CUTSCENE(plParam0, "SET_INPUT_EVENT");
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(6);
+GRAPHICS::END_SCALEFORM_MOVIE_METHOD();
+}
+if(GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 188)){
+NETWORK::NETWORK_LEAVE_PED_BEHIND_BEFORE_CUTSCENE(plParam0, "SET_INPUT_EVENT");
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(8);
+GRAPHICS::END_SCALEFORM_MOVIE_METHOD();
+}
+if(GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 187)){
+NETWORK::NETWORK_LEAVE_PED_BEHIND_BEFORE_CUTSCENE(plParam0, "SET_INPUT_EVENT");
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(9);
+GRAPHICS::END_SCALEFORM_MOVIE_METHOD();
+}
+if(GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 201)){
+NETWORK::NETWORK_LEAVE_PED_BEHIND_BEFORE_CUTSCENE(plParam0, "SET_INPUT_EVENT");
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(16);
+GRAPHICS::END_SCALEFORM_MOVIE_METHOD();
+}
+if(GRAPHICS::RESET_PARTICLE_FX_OVERRIDE(2, 202)){
+NETWORK::NETWORK_LEAVE_PED_BEHIND_BEFORE_CUTSCENE(plParam0, "SET_INPUT_EVENT");
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(17);
+GRAPHICS::END_SCALEFORM_MOVIE_METHOD();
+}}
+return;
+}
+
+
+void func_2() // Position - 0x215{
+func_3(false);
+MISC::SET_GAME_PAUSED(false);
+GRAPHICS::SET_SCALEFORM_MOVIE_AS_NO_LONGER_NEEDED(&plLocal_18);
+SCRIPT::TERMINATE_THIS_THREAD();
+return;
+}
+
+
+void func_3(BOOL bParam0) // Position - 0x231{
+if(bParam0){
+func_10();
+if(Global_20383.f_1==10 || Global_20383.f_1==9) MISC::SET_BIT(&Global_8254, 16);
+Global_20383.f_1=1;
+if(func_9(0)) func_4(0);
+}elseif(Global_20383.f_1==1){
+if(!Global_20383.f_1==0) Global_20383.f_1=3;
+}
+return;
+}
+
+
+void func_4(int iParam0) // Position - 0x294{
+if(func_8()) return;
+if(Global_20584)if(func_7()) func_6(true, true);
+else func_6(false, false);
+if(Global_20383.f_1==10 || Global_20383.f_1==9) MISC::SET_BIT(&Global_8254, 16);
+if(AUDIO::IS_MOBILE_PHONE_CALL_ONGOING()) AUDIO::STOP_SCRIPTED_CONVERSATION(false);
+Global_21725=5;
+if(iParam0==1) MISC::SET_BIT(&Global_8253, 30);
+else MISC::CLEAR_BIT(&Global_8253, 30);
+if(!func_5()) Global_20383.f_1=3;
+return;
+}
+BOOL func_5() // Position - 0x31E{
+if(Global_20383.f_1==1 || Global_20383.f_1==0) return true;
+return false;
+}
+
+
+void func_6(BOOL bParam0, BOOL bParam1) // Position - 0x345{
+if(bParam0){
+if(func_9(0)){
+Global_20584=true;
+if(bParam1) MOBILE::GET_MOBILE_PHONE_POSITION(&Global_20320);
+Global_20311={
+Global_20329[Global_20328 /*3*/] 
+};
+MOBILE::SET_MOBILE_PHONE_POSITION(Global_20311);
+}}elseif(Global_20584==true){
+Global_20584=false;
+Global_20311={
+Global_20336[Global_20328 /*3*/] 
+};
+if(bParam1) MOBILE::SET_MOBILE_PHONE_POSITION(Global_20320);
+else MOBILE::SET_MOBILE_PHONE_POSITION(Global_20311);
+}
+return;
+}
+BOOL func_7() // Position - 0x3B9{
+return IS_BIT_SET(Global_1962996, 5);
+}
+BOOL func_8() // Position - 0x3C7{
+return IS_BIT_SET(Global_1962996, 19);
+}
+BOOL func_9(int iParam0) // Position - 0x3D6{
+if(iParam0==1)if(Global_20383.f_1 > 3)if(IS_BIT_SET(Global_8253, 14)) return true;
+else return false;
+else return false;
+if(SCRIPT::GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(joaat("cellphone_flashhand")) > 0) return true;
+if(Global_20383.f_1 > 3) return true;
+return false;
+}
+
+
+void func_10() // Position - 0x42D{
+if(Global_20383.f_1==9 || Global_20383.f_1==10){
+Global_21778=0;
+Global_21774=1;
+}
+return;
+}
