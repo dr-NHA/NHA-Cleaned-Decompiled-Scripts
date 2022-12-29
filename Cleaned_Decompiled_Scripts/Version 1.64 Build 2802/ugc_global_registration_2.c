a91a914cf5147b7198e84dcbed6865ee944a8c78
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2022-12-29 10:43:10 +1030

    ugc_global_registration_2.c Uploaded @ 29/12/2022 12:13:02 AM

---
+++ Cleaned_Decompiled_Scripts/Version 1.64 Build 2802/ugc_global_registration_2.c
@@ -0,0 +1,18 @@
+//Cleaned With dr NHA's C Script Cleaner V2.72
+// Program registers 9817 globals at index 4 starting from Global_1048576
+
+void main() // Position - 0x0{
+Global_1057409=-1;
+Global_1058358={
+10.61f, 0.11f, 210.2f 
+};
+Global_1058361={
+0f, 0f, 0f 
+};
+Global_1058364=32;
+Global_1058365=5000;
+Global_1058366=1;
+Global_1058367=-1;
+Global_1058368=1;
+return;
+}
