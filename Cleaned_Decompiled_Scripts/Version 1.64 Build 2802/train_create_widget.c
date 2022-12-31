78a738032e4023c6dc86358235c4c10c6095a124
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2023-01-01 02:28:46 +1030

    train_create_widget.c Uploaded @ 31/12/2022 3:58:41 PM

---
+++ Cleaned_Decompiled_Scripts/Version 1.64 Build 2802/train_create_widget.c
@@ -0,0 +1,70 @@
+//Cleaned With dr NHA's C Script Cleaner V2.8
+#region Local Var
+var uLocal_0=0;
+struct<3> Local_1={
+0, 0, 0 
+};
+var uLocal_4=0;
+float fLocal_5=0f;
+int iLocal_6=0;
+int iLocal_7=0;
+bool bLocal_8=0;
+int iLocal_9=0;
+#endregion
+
+void __EntryFunction__(){
+Local_1={
+613f, 6438f, 31f 
+};
+fLocal_5=5f;
+iLocal_6=1;
+MISC::SET_MISSION_FLAG(1);
+if(PLAYER::HAS_FORCE_CLEANUP_OCCURRED(3)){
+func_1();
+}
+VEHICLE::SET_RANDOM_TRAINS(0);
+VEHICLE::DELETE_ALL_TRAINS();
+if(!PED::IS_PED_INJURED(PLAYER::PLAYER_PED_ID())){
+ENTITY::SET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), 626.68f, 6442.31f, 30.88f, 1, 0, 0, 1);
+ENTITY::SET_ENTITY_HEADING(PLAYER::PLAYER_PED_ID(), -177f);
+CAM::SET_GAMEPLAY_CAM_RELATIVE_HEADING(0);
+}
+STREAMING::REQUEST_MODEL(joaat("freight"));
+STREAMING::REQUEST_MODEL(joaat("freightcar"));
+STREAMING::REQUEST_MODEL(joaat("freightgrain"));
+STREAMING::REQUEST_MODEL(joaat("freightcont1"));
+STREAMING::REQUEST_MODEL(joaat("freightcont2"));
+STREAMING::REQUEST_MODEL(joaat("tankercar"));
+STREAMING::REQUEST_MODEL(joaat("metrotrain"));
+while ((((((!STREAMING::HAS_MODEL_LOADED(joaat("freight")) || !STREAMING::HAS_MODEL_LOADED(joaat("freightcar"))) || !STREAMING::HAS_MODEL_LOADED(joaat("freightgrain"))) || !STREAMING::HAS_MODEL_LOADED(joaat("freightcont1"))) || !STREAMING::HAS_MODEL_LOADED(joaat("freightcont2"))) || !STREAMING::HAS_MODEL_LOADED(joaat("tankercar"))) || !STREAMING::HAS_MODEL_LOADED(joaat("metrotrain"))){
+SYSTEM::WAIT(0);
+}
+while (true){
+SYSTEM::WAIT(0);
+if(!PED::IS_PED_INJURED(PLAYER::PLAYER_PED_ID())){
+if(iLocal_7){
+if(ENTITY::DOES_ENTITY_EXIST(uLocal_0)){
+VEHICLE::DELETE_MISSION_TRAIN(&uLocal_0);
+}
+uLocal_0=VEHICLE::CREATE_MISSION_TRAIN(uLocal_4, Local_1, iLocal_6, 0, 0);
+iLocal_7=0;
+}
+if(iLocal_9){
+Local_1={
+CAM::GET_FINAL_RENDERED_CAM_COORD() 
+};
+iLocal_9=0;
+}
+if(VEHICLE::IS_VEHICLE_DRIVEABLE(uLocal_0, 0) && !ENTITY::IS_ENTITY_DEAD(uLocal_0, 0)){
+VEHICLE::SET_TRAIN_SPEED(uLocal_0, fLocal_5);
+VEHICLE::SET_TRAIN_CRUISE_SPEED(uLocal_0, fLocal_5);
+}
+if(bLocal_8){
+func_1();
+}}}}
+
+
+void func_1(){
+VEHICLE::SET_RANDOM_TRAINS(1);
+SCRIPT::TERMINATE_THIS_THREAD();
+}
