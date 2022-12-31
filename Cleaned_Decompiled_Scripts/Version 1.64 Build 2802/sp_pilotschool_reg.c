4e5471f97b2f139012d119fd80ecb6d760f0a882
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2023-01-01 02:27:19 +1030

    sp_pilotschool_reg.c Uploaded @ 31/12/2022 3:57:17 PM

---
+++ Cleaned_Decompiled_Scripts/Version 1.64 Build 2802/sp_pilotschool_reg.c
@@ -0,0 +1,14 @@
+//Cleaned With dr NHA's C Script Cleaner V2.8
+
+
+void __EntryFunction__(){
+MISC::START_SAVE_DATA(&Global_3145728, 1, 1);
+func_1();
+MISC::STOP_SAVE_DATA();
+MISC::SET_BIT(&Global_4543155, 1);
+}
+
+
+void func_1(){
+MISC::REGISTER_INT_TO_SAVE(&Global_3145728, "PILOT_SCHOOL_DUMMY_DATA");
+}
