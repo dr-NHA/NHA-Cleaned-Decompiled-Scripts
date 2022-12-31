707f5881b8845fa2f24c278f916ed42c5889e9d5
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2023-01-01 02:27:13 +1030

    sp_dlc_registration.c Uploaded @ 31/12/2022 3:57:11 PM

---
+++ Cleaned_Decompiled_Scripts/Version 1.64 Build 2802/sp_dlc_registration.c
@@ -0,0 +1,13 @@
+//Cleaned With dr NHA's C Script Cleaner V2.8
+
+
+void __EntryFunction__(){
+int iVar0;
+int iVar1;
+MISC::START_SAVE_DATA(&Global_2883692, 1, 1);
+MISC::REGISTER_INT_TO_SAVE(&Global_2883692, "aFakeSavedInt");
+MISC::STOP_SAVE_DATA();
+MISC::SET_BIT(&Global_4543155, 0);
+iVar0=1;
+iVar1=MISC::GET_SIZE_OF_SAVE_DATA(1);
+if(iVar0 !=iVar1){}}
